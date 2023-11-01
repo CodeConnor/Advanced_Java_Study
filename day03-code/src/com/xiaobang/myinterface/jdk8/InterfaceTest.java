57cade9cd1993715150a41135d36b40744e3dd61
Connor <xiaobang199806@163.com>
2023-11-01 10:29:55 +0800

    add test

---
+++ day03-code/src/com/xiaobang/myinterface/jdk8/InterfaceTest.java
@@ -0,0 +1,56 @@
+package com.xiaobang.myinterface.jdk8;
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        AInterImpl A = new AInterImpl();
+        A.method();
+    }
+}
+interface AAA {
+    default void method() {
+        System.out.println("AAA...method...");
+    }
+}
+
+interface Inter {
+    void show();
+    void print();
+
+    default void method () {
+        System.out.println("Inter...method...");
+    }
+}
+
+class AInterImpl implements Inter, AAA {
+
+    @Override
+    public void method() {
+        Inter.super.method();
+        AAA.super.method();
+    }
+
+    @Override
+    public void show() {
+        System.out.println("A...show...");
+    }
+
+    @Override
+    public void print() {
+        System.out.println("A...print...");
+    }
+
+}
+
+
+class BInterImpl implements Inter {
+
+    @Override
+    public void show() {
+        System.out.println("B...show...");
+    }
+
+    @Override
+    public void print() {
+        System.out.println("B...print...");
+    }
+}
