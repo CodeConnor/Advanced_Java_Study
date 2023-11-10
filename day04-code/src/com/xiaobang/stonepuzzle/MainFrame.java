dcfe35d27fb221b6e40b42912bbbf6a8dd9f840f
Connor <xiaobang199806@163.com>
2023-11-10 19:02:49 +0800

    update test

--- day04-code/src/com/xiaobang/stonepuzzle/MainFrame.java
+++ day04-code/src/com/xiaobang/stonepuzzle/MainFrame.java
@@ -1,8 +1,11 @@
 package com.xiaobang.stonepuzzle;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements KeyListener {
 
     // 定义二维数组, 用该数组当做图片的指针
     int[][] data = {
@@ -13,8 +16,13 @@ public class MainFrame extends JFrame {
     };
 
     public MainFrame() {
+        // 监听键盘输入
+        // 窗体对象.addKeyLisener(KeyLisener实现类对象)
+        this.addKeyListener(this);
         // 初始化窗体
         initFrame();
+        // 打乱方块
+        initData();
         // 绘制界面
         paintView();
         // 窗体可见
@@ -32,6 +40,26 @@ public class MainFrame extends JFrame {
         setAlwaysOnTop(true);  // 窗体置顶
         setLocationRelativeTo(null);  // 设置窗口居中
         setLayout(null); // 取消默认布局, 防止图片插入充满窗口
+    }
+
+    /**
+     * 初始化数据(打乱方块, 打乱数组元素)
+     */
+    public void initData() {
+        Random r = new Random();
+        // 遍历数组中的元素
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                // 获取随机索引
+                int randomX = r.nextInt(4);
+                int randomY = r.nextInt(4);
+
+                // 交换数组中的元素
+                int temp = data[i][j];
+                data[i][j] = data[randomX][randomY];
+                data[randomX][randomY] = temp;
+            }
+        }
     }
 
     /**
@@ -56,4 +84,41 @@ public class MainFrame extends JFrame {
         background.setBounds(26, 30, 450, 484);
         getContentPane().add(background);
     }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // 获取键盘按键的ASCII码值
+        int keyCode = e.getKeyCode();
+        // 移动方块
+        moveCube(keyCode);
+
+    }
+
+    /**
+     * 此方法用于移动方块的业务
+     */
+    private static void moveCube(int keyCode) {
+        if (keyCode == 37) {
+            System.out.println("左移动");
+        } else if (keyCode == 38) {
+            System.out.println("上移动");
+        } else if (keyCode == 39) {
+            System.out.println("右移动");
+        } else if (keyCode == 40) {
+            System.out.println("下移动");
+        }
+    }
+
+    // -------------------------------------------------
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+    // -------------------------------------------------
 }
