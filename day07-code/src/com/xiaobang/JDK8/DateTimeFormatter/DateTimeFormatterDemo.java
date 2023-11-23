f46fdfb695b35abaaeb6c281ffd488f65af3d1bd
Connor <xiaobang199806@163.com>
2023-11-23 17:54:37 +0800

    add demo

--- day07-code/src/com/xiaobang/JDK8/DateTimeFormatter/DateTimeFormatterDemo.java
+++ day07-code/src/com/xiaobang/JDK8/DateTimeFormatter/DateTimeFormatterDemo.java
@@ -1,2 +1,20 @@
-package com.xiaobang.JDK8.DateTimeFormatter;public class DateTimeFormatterDemo {
+package com.xiaobang.JDK8.DateTimeFormatter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterDemo {
+    public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年M月d日");
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        String format = dateTimeFormatter.format(now);
+        System.out.println(format);
+
+        // 解析
+        LocalDate parse = LocalDate.parse(format, dateTimeFormatter);
+        System.out.println(parse);
+    }
 }
