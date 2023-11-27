9dd163be0ccbc26d7539f77e1834d80f9a8bf557
Connor <xiaobang199806@163.com>
2023-11-27 11:44:34 +0800

    add jar

---
+++ JDBC-code/src/com/xiaobang/drivermanager/DriverManagerDemo1.java
@@ -0,0 +1,16 @@
+package com.xiaobang.drivermanager;
+
+import java.sql.*;
+
+
+public class DriverManagerDemo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void show1() {
+        // 注册驱动
+        
+    }
+}
