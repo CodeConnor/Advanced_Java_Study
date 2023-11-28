970d6d87ceb088ef67728d020bbd21a97b392541
Connor <xiaobang199806@163.com>
2023-11-28 18:12:42 +0800

    add demo

--- JDBC-code/src/com/xiaobang/api/preparedstatement/PSCURDDemo.java
+++ JDBC-code/src/com/xiaobang/api/preparedstatement/PSCURDDemo.java
@@ -3,6 +3,10 @@ package com.xiaobang.api.preparedstatement;
 import org.junit.Test;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 使用PreparedStatement进行t_user表的curd动作
@@ -115,25 +119,55 @@ public class PSCURDDemo {
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/db_jdbc", "root", "123456");
 
         // 编写sql语句
-        String sql = "select * from t_user where account = ?;";
+        String sql = "select id, account, PASSWORD, nickname from t_user;";
 
         // 创建preparedStatement
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-        // 对占位符赋值
-        preparedStatement.setObject(1, "test");
-
         // 发送SQL语句, 返回执行结果
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        // 解析结果集
+        // 解析结果集, 使用list<map>结构存储结果并输出
+        // 创建list
+        List<Map> list = new ArrayList<>();
+
+        // 获取结果集的元数据
+        ResultSetMetaData metaData = resultSet.getMetaData();
+
+        // 获取元数据中的列数
+        int columnCount = metaData.getColumnCount();
+
         while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String account = resultSet.getString("account");
-            String password = resultSet.getString("PASSWORD");
-            String nickname = resultSet.getString("nickname");
-            System.out.println(id+"--"+account+"--"+password+"--"+nickname);
+            // 创建Map
+            Map map = new HashMap();
+            // 将结果通过键值对的形式存入map
+            /*
+                手动解析结果集, 不灵活, 不推荐
+            map.put("id", resultSet.getInt("id"));
+            map.put("account", resultSet.getString("account"));
+            map.put("password", resultSet.getString("PASSWORD"));
+            map.put("nickname", resultSet.getString("nickname"));
+            */
+            // 遍历结果集的列, 结果集的索引从1开始
+            for (int i = 1; i <= columnCount; i++) {
+                // 获取列的值, 使用Object类接收可以接收任意类型的值
+                Object value = resultSet.getObject(i);
+
+                // 获取列名称, 使用getColumnLabel可获取列的别名且别名优先
+                // 而使用getColunName只能获取列的原名称无法获取别名, 推荐使用Label
+                String columnLabel = metaData.getColumnLabel(i);
+
+                // 将列名和列的值作为键值对添加到map中
+                map.put(columnLabel, value);
+
+            }
+
+            // 将map添加到list中
+            list.add(map);
+
         }
+        // 输出结果
+        System.out.println(list);
 
         // 关闭资源
         resultSet.close();
