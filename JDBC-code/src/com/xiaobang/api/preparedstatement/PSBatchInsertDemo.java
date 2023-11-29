577654d35972a31260514f37654ad3856e75f5f2
Connor <xiaobang199806@163.com>
2023-11-29 13:35:45 +0800

    update demo

--- JDBC-code/src/com/xiaobang/api/preparedstatement/PSBatchInsertDemo.java
+++ JDBC-code/src/com/xiaobang/api/preparedstatement/PSBatchInsertDemo.java
@@ -16,8 +16,8 @@ public class PSBatchInsertDemo {
         String sql = "insert into t_user(account,PASSWORD,nickname) values(?,?,?)";
         // 创建PreparedStatement对象
         PreparedStatement statement = connection.prepareStatement(sql);
-
-
+        // 计算消耗时间
+        long start = System.currentTimeMillis();
         // 插入10000条数据
         for (int i = 0; i < 10000; i++) {
             // 设置参数
@@ -27,10 +27,47 @@ public class PSBatchInsertDemo {
             // 执行SQL
             statement.executeUpdate();
         }
+        long end = System.currentTimeMillis();
+        System.out.println("共消耗了" + (end - start) / 1000 + "秒");
 
         // 释放资源
         statement.close();
         connection.close();
 
+    }
+
+
+    @Test
+    public void batchInsert() throws ClassNotFoundException, SQLException {
+        // 注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 连接数据库
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/db_jdbc?rewriteBatchedStatements=true",
+                "root", "123456");
+        // 创建SQL
+        String sql = "insert into t_user(account,PASSWORD,nickname) values (?,?,?)";
+        // 创建PreparedStatement对象
+        PreparedStatement statement = connection.prepareStatement(sql);
+        // 计算消耗时间
+        long start = System.currentTimeMillis();
+        // 插入10000条数据
+        for (int i = 0; i < 10000; i++) {
+            // 设置参数
+            statement.setString(1, "testt" + i);
+            statement.setString(2, "testt" + i);
+            statement.setString(3, "测试测试测试员" + i);
+            // 不执行, 将数据追加到values后面
+            statement.addBatch();
+        }
+        // 执行批量操作
+        statement.executeBatch();
+
+        long end = System.currentTimeMillis();
+        System.out.println("共消耗了" + (end - start) + "毫秒");
+
+        // 释放资源
+        statement.close();
+        connection.close();
     }
 }
