f69e14fda8af2651654b12ea00a45e396378d49c
Connor <xiaobang199806@163.com>
2023-12-15 16:28:43 +0800

    add demo

---
+++ day07-code/src/com/xiaobang/JDK8/ZoneId/ZoneIdDemo1.java
@@ -0,0 +1,27 @@
+package com.xiaobang.JDK8.ZoneId;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+public class ZoneIdDemo1 {
+    public static void main(String[] args) {
+        // 获取Java中支持的所有时区
+        Set<String> set = ZoneId.getAvailableZoneIds();
+        System.out.println(set);
+        System.out.println(set.size());
+
+        // 获取系统默认时区对象
+        ZoneId sys = ZoneId.systemDefault();
+        System.out.println(sys);
+
+        // 获取指定时区对象
+        ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+        System.out.println(zoneId);
+
+        // 使用时区对象生成时间戳
+        ZonedDateTime zonedDateTime = Instant.now().atZone(zoneId);
+        System.out.println(zonedDateTime);
+    }
+}
