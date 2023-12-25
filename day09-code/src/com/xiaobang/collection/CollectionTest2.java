b96be1b795d7072022f6875a50e582724c7d0da9
Connor <xiaobang199806@163.com>
2023-12-25 19:15:44 +0800

    add test

---
+++ day09-code/src/com/xiaobang/collection/CollectionTest2.java
@@ -0,0 +1,26 @@
+package com.xiaobang.collection;
+
+import com.xiaobang.domain.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionTest2 {
+    public static void main(String[] args) {
+
+        Collection<Student> c = new ArrayList<>();
+
+        c.add(new Student("lihua", 20));
+        c.add(new Student("xiaoming", 21));
+        c.add(new Student("xiaohua", 22));
+
+        // 生成迭代器
+        Iterator<Student> it = c.iterator();
+
+        // 遍历
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
