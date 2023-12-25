e78c03fc4c35e22411bf9c8403d5ab119760555d
Connor <xiaobang199806@163.com>
2023-12-25 18:17:05 +0800

    update test

--- day09-code/src/com/xiaobang/collection/CollectionTest1.java
+++ day09-code/src/com/xiaobang/collection/CollectionTest1.java
@@ -1,11 +1,27 @@
 package com.xiaobang.collection;
 
+import com.xiaobang.domain.Student;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class CollectionTest1 {
     public static void main(String[] args) {
 
+        Collection<Student> c = new ArrayList<>();
+
+        c.add(new Student("lihua", 20));
+        c.add(new Student("xiaoming", 21));
+        c.add(new Student("xiaohua", 22));
+
+        c.remove(new Student("lihua",20));  // 依赖于equals方法, 对对象做删除时需要重写equals方法
+        System.out.println(c);
+
+
+
+    }
+
+    private static void method() {
         Collection<String> c = new ArrayList<>();
 
         c.add("aaa");
