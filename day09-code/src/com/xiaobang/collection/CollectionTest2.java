9272c790b63640e564b1c3a1fdaaab65492433dd
Connor <xiaobang199806@163.com>
2023-12-26 14:36:37 +0800

    update test

--- day09-code/src/com/xiaobang/collection/CollectionTest2.java
+++ day09-code/src/com/xiaobang/collection/CollectionTest2.java
@@ -5,6 +5,7 @@ import com.xiaobang.domain.Student;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.function.Consumer;
 
 public class CollectionTest2 {
     public static void main(String[] args) {
@@ -18,10 +19,25 @@ public class CollectionTest2 {
         // 生成迭代器
         Iterator<Student> it = c.iterator();
 
-        // 遍历
+        // 迭代器遍历
         while (it.hasNext()) {
             Student stu = it.next();
             System.out.println(stu.getName() + "---" + stu.getAge());
         }
+        System.out.println("--------------------------------");
+        // 使用增强for循环遍历
+        for (Student stu : c) {
+            System.out.println(stu.getName() + "---" + stu.getAge());
+        }
+
+        System.out.println("-------------------------------");
+
+        // 使用foreach方法
+        c.forEach(new Consumer<Student>() {
+            @Override
+            public void accept(Student stu) {
+                System.out.println(stu.getName() + "---" + stu.getAge());
+            }
+        });
     }
 }
