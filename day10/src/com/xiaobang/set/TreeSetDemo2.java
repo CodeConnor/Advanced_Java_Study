fa4c4647d82be303fdd86d1eff6c75bce95500e2
Connor <xiaobang199806@163.com>
2024-01-08 12:20:57 +0800

    add demo

---
+++ day10/src/com/xiaobang/set/TreeSetDemo2.java
@@ -0,0 +1,18 @@
+package com.xiaobang.set;
+
+import com.xiaobang.doamin.Student;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo2 {
+    public static void main(String[] args) {
+        TreeSet<Student> ts = new TreeSet<Student>();
+
+        ts.add(new Student("xiaoming", 18));
+        ts.add(new Student("lihua", 19));
+        ts.add(new Student("xiaohua", 20));
+        ts.add(new Student("xiaogang", 21));
+
+        System.out.println(ts);
+    }
+}
