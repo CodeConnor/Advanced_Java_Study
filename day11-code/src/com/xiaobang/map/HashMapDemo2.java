bff711e54dce4f15858b6647876a75c29fc7edc1
Connor <xiaobang199806@163.com>
2024-01-19 17:41:12 +0800

    update demo

--- day11-code/src/com/xiaobang/map/HashMapDemo2.java
+++ day11-code/src/com/xiaobang/map/HashMapDemo2.java
@@ -3,6 +3,7 @@ package com.xiaobang.map;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiConsumer;
 
 public class HashMapDemo2 {
     public static void main(String[] args) {
@@ -11,6 +12,15 @@ public class HashMapDemo2 {
         hs.put("xiaozhang", "beijing");
         hs.put("lihua", "guangzhou");
 
+        hs.forEach(new BiConsumer<String, String>() {
+            @Override
+            public void accept(String key, String value) {
+                System.out.println(key + "---" + value);
+            }
+        });
+    }
+
+    private static void method(HashMap<String, String> hs) {
         Set<Map.Entry<String, String>> entries = hs.entrySet();
         System.out.println(entries);
 
